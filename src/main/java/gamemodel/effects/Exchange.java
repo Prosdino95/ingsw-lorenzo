1fbe63f5ff135b04c69e958019dcefebbac7d641
Davide Palesano <davide.palesano@gmail.com>
2017-05-28 17:56:21 +0200

    Exchange class 2.0
    aggiunti altri effetti

--- src/main/java/gamemodel/effects/Exchange.java
+++ src/main/java/gamemodel/effects/Exchange.java
@@ -12,54 +12,15 @@ public class Exchange implements Effect
 	private Resource resourcesOut;
 	private Effect councilPrivilegesIn;
 	
-	public Exchange(Resource resourcesIn,Resource resourcesOut)
+	public Exchange(Point pointsIn,Point pointsOut,Resource resourcesIn,Resource resourcesOut,Effect councilPrivilegesIn)
 	{
 		this.resourcesIn=resourcesIn;
 		this.resourcesOut=resourcesOut;
-		this.pointsIn=null;
-		this.pointsOut=null;
-		this.councilPrivilegesIn=null;
-	}
-	public Exchange(Point pointsIn,Point pointsOut)
-	{
 		this.pointsIn=pointsIn;
 		this.pointsOut=pointsOut;
-		this.resourcesIn=null;
-		this.resourcesOut=null;
-		this.councilPrivilegesIn=null;
-	}
-	public Exchange(Point pointsIn,Resource resourcesOut)
-	{
-		this.pointsIn=pointsIn;
-		this.pointsOut=null;
-		this.resourcesIn=null;
-		this.resourcesOut=resourcesOut;
-		this.councilPrivilegesIn=null;
-	}
-	public Exchange(Resource resourcesIn,Point pointsOut)
-	{
-		this.pointsIn=null;
-		this.pointsOut=pointsOut;
-		this.resourcesIn=resourcesIn;
-		this.resourcesOut=null;
-		this.councilPrivilegesIn=null;
-	}
-	public Exchange(Effect councilPrivilegesIn,Resource resourcesOut)
-	{
-		this.pointsIn=null;
-		this.pointsOut=null;
-		this.resourcesIn=null;
-		this.resourcesOut=resourcesOut;
 		this.councilPrivilegesIn=councilPrivilegesIn;
 	}
-	public Exchange(Effect councilPrivilegesIn,Point pointsOut)
-	{
-		this.pointsIn=null;
-		this.pointsOut=pointsOut;
-		this.resourcesIn=null;
-		this.resourcesOut=null;
-		this.councilPrivilegesIn=councilPrivilegesIn;
-	}
+	
 	
 	@Override
 	public void activate(Player player)
@@ -94,6 +55,30 @@ public class Exchange implements Effect
 			player.subResources(this.resourcesOut);
 			this.councilPrivilegesIn.activate(player);
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+		if(this.pointsIn==null && this.pointsOut==null && this.resourcesIn==null)
+		{
+			player.subResources(this.resourcesOut);
+			this.councilPrivilegesIn.activate(player);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
 
