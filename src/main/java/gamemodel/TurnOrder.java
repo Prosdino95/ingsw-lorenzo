acf36db3f92e561e6302cfa56cdcb4250f014854
Marco Prosdocimi <prosdino95@gmail.com>
2017-06-24 20:59:22 +0200

    turn order Test

--- src/main/java/gamemodel/TurnOrder.java
+++ src/main/java/gamemodel/TurnOrder.java
@@ -21,6 +21,7 @@ public class TurnOrder{
 	public void setupRound(List<Player> list){
 		if(list.isEmpty()){
 			this.iterator=actionOrder.iterator();
+			controlSlide(playerInGame);
 			return;
 		}	
 		list.addAll(playerInGame);
@@ -29,6 +30,10 @@ public class TurnOrder{
 		.distinct()
 		.collect(Collectors.toList());		
 		generateActionOrder(players);
+		controlSlide(playerInGame);
+	}
+	
+	private void controlSlide(List<Player> players){
 		for(Player p:players)
 			if(!p.getPEffects(PEffect.NO_FIRST_ACTION).isEmpty())
 				actionOrder.slide(p);
@@ -62,56 +67,7 @@ public class TurnOrder{
 	public List<Player> getListActionOrder(){
 		return actionOrder.getList();
 	}
-	//TODO il mega main poi va via, lasciamolo ora lo usiamo per il debug
-	public static void main(String[] args){
-		List<Player> players=new ArrayList<>();
-		Player p=new Player(null, null, Team.BLUE);
-		Player p1=new Player(null, null, Team.RED);
-		//Player p2=new Player(null, null, Team.GREEN);
-		//Player p3=new Player(null, null, Team.YELLOW);
-		players.add(p);
-		players.add(p1);	
-		//players.add(p2);
-		//players.add(p3);
-		TurnOrder to=new TurnOrder(players);
-		System.out.println("turni iniziali"+to);
-		System.out.println("primo "+to.getNextPlayer());
-		System.out.println("secondo "+to.getNextPlayer());
-		System.out.println("terzo "+to.getNextPlayer());
-		System.out.println("... ");
-		List<Player>place=new ArrayList<>();
-		place.add(p1);
-		place.add(p1);
-		place.add(p);
-		place.add(p);
-		System.out.println("palazzo del consiglio: "+place);
-		to.setupRound(place);
-		System.out.println("nuovi turni: "+to);
-		System.out.println("primo "+to.getNextPlayer());
-		System.out.println("secondo "+to.getNextPlayer());
-		System.out.println("terzo "+to.getNextPlayer());
-		System.out.println("... ");
-		place=new ArrayList<>();
-		//place.add(p3);
-		System.out.println("palazzo del consiglio: "+place);
-		to.setupRound(place);
-		System.out.println("nuovi turni: "+to);
-		place=new ArrayList<>();
-		System.out.println("palazzo del consiglio: "+place);
-		to.setupRound(place);
-		System.out.println("nuovi turni: "+to);
-		System.out.println("primo "+to.getNextPlayer());
-		System.out.println("secondo "+to.getNextPlayer());
-		System.out.println("terzo "+to.getNextPlayer());
-		System.out.println("... ");
-		System.out.println("test effetto giocatore Red");
-		System.out.println("turni: "+to);
-		System.out.println();
-		to.actionOrder.slide(p);
-		System.out.println("turni: "+to);
-	}
-}
-
+}	
 
 class ActionOrder {
 	
