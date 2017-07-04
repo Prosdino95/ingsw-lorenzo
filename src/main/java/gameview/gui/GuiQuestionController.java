44ddccc027809c79c9dfc3f4c8aff69600f44697
Marco Prosdocimi <prosdino95@gmail.com>
2017-07-04 10:12:14 +0200

    front end guiQuestion

--- src/main/java/gameview/gui/GuiQuestionController.java
+++ src/main/java/gameview/gui/GuiQuestionController.java
@@ -2,16 +2,23 @@ package gameview.gui;
 
 import java.util.List;
 
+
 import gamemodel.GameQuestion;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import reti.ServerResponse;
@@ -20,12 +27,19 @@ public class GuiQuestionController {
 	
 	@FXML TextFlow text;
 	@FXML VBox question;
+	@FXML TextField servants;
+	@FXML TextFlow servantsQuest;
+	@FXML AnchorPane root;
 	private ToggleGroup choices;
 	private ServerResponse sr;
 	private String answer;
 	
 	public void update(ServerResponse sr){
+		root.setBackground(new Background(new BackgroundImage(new Image("/wood.jpg"), null, null, null, null)));
+		servants.setEditable(false);
 		choices=new ToggleGroup();
+		question.setSpacing(-20);
+		question.setStyle("-fx-background-color: transparent;");
 		choices.selectedToggleProperty().addListener(new ChangeListener<Toggle>(){
 			@Override
 			public void changed(ObservableValue<? extends Toggle> ov, Toggle old_toggle, Toggle new_toggle) {
@@ -48,13 +62,14 @@ public class GuiQuestionController {
 		case VATICAN_SUPPORT:
 			break;
 		default:
-			break;
-		
+			break;		
 		}
 	}
 
 	private void initializeQuestion() {		
-		text.getChildren().add(new Text(sr.getQuestion().getGq().toString()));	
+		Text text=new Text(sr.getQuestion().getGq().toString());
+		this.text.setStyle("-fx-text-fill: white; -fx-font-size: 20px;");
+		this.text.getChildren().add(text);	
 		if(sr.getQuestion().getGq()==GameQuestion.HOW_MANY_FMS)
 			questionHowMany();
 		else
@@ -67,32 +82,49 @@ public class GuiQuestionController {
 		HBox hbox =null;
 		for(Object o:list){
 			VBox vbox=new VBox();
+			vbox.setStyle("-fx-background-color: transparent;");
 			RadioButton rbutton=new RadioButton();
 			if(id%4==0){
 				hbox=new HBox();
+				hbox.setSpacing(20);
+				hbox.setStyle("-fx-background-color: transparent;");
 				question.getChildren().add(hbox);
 			}				
 			if(id==0)
 				rbutton.setSelected(true);
 			rbutton.setId(id.toString());
 			rbutton.setToggleGroup(choices);
-			Text text=new Text(o.toString());			
+			Text chooseText=new Text(o.toString());	
+			chooseText.setFont(Font.font(7));
 			vbox.getChildren().add(rbutton);
-			vbox.getChildren().add(new TextFlow(text));
+			TextFlow textFlow=new TextFlow(chooseText);
+			double top=12;
+			double left=50;
+			textFlow.setPadding(new Insets(top,left, 0, 0));
+			textFlow.setPrefSize(153, 146);
+			vbox.getChildren().add(textFlow);
 			hbox.getChildren().add(vbox);
 			id++;
-		}			
+		}	
 	}
 
 	private void questionHowMany() {
-		Text t=new Text("   write the number and press ENTER");
-		text.getChildren().add(t);
-		TextField tf=new TextField();
-		question.getChildren().add(tf);
-		tf.setOnKeyPressed(e -> {
+		answer="0";
+		Text t=new Text("write the number and press ENTER");
+		servantsQuest.getChildren().add(t);
+		servants.setEditable(true);
+		servants.setOnKeyPressed(e -> {
 			switch (e.getCode()) {
 			case ENTER:
-				answer=tf.getText();
+				try{
+					Integer.parseInt(servants.getText());
+					answer=servants.getText();
+				}
+				catch(NumberFormatException ex){
+					t.setText(" so you must write a NUMBER and press ENTER");
+				}
+				answer=servants.getText();
+				break;
 			default:
 				break;
 			}
--- src/main/java/gameview/gui/Test.java
+++ src/main/java/gameview/gui/Test.java
@@ -29,11 +29,22 @@ public class Test extends Application{
 			Pane root = loader.load();
 			GuiQuestionController controller=loader.getController();
 			creacarte();
+			//creaaltro();
 			//controller.update(new ServerResponse(GameError.RESOURCE_ERR_SERVANTS));
-			//controller.update(new ServerResponse(new Question(GameQuestion.CHOOSE_FAMILY_MEMBER,choose)));
+			//controller.update(new ServerResponse(new Question(GameQuestion.LEADER,choose)));
 			controller.update(new ServerResponse(new Question(GameQuestion.HOW_MANY_FMS , new ArrayList<>())));	
 			stage.setScene(new Scene(root));
 			stage.show();
+		}
+		private void creaaltro() {
+			choose.add(new Resource(5,5,5,5));
+			choose.add(new Resource(5,5,5,5));
+			choose.add(new Resource(5,5,5,5));
+			choose.add(new Resource(5,5,5,5));
+			
+			choose.add(new Resource(5,5,5,5));
+			
+			
 		}
 		private void creacarte() {
 			List<IstantEffect> l=new ArrayList<>();
@@ -41,10 +52,12 @@ public class Test extends Application{
 			CharactersCard r1=new CharactersCard(1,"name",1,new Resource(1,1,1,1),new Resource(1,1,1,1),new Point(1,1,1),new Point(1,1,1),l,null,CardType.CHARACTER);
 			CharactersCard r2=new CharactersCard(2,"name",1,new Resource(1,1,1,1),new Resource(1,1,1,1),new Point(1,1,1),new Point(1,1,1),l,null,CardType.CHARACTER);
 			CharactersCard r3=new CharactersCard(3,"name",1,new Resource(1,1,1,1),new Resource(1,1,1,1),new Point(1,1,1),new Point(1,1,1),l,null,CardType.CHARACTER);
+			for(int i=0;i<3;i++){
 			choose.add(r);
 			choose.add(r1);
 			choose.add(r2);
 			choose.add(r3);
+			}
 		}
 		public static void main(String args[]){
 			Application.launch(Test.class, args);		
